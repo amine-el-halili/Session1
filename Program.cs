599738b9a317cd13aafb94edb99f612563cd399a
elhalili <aminhalili2002@gmail.com>
2022-10-25 09:21:56 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main()
+    {
+        string input = "";
+        int num = -1;
+        
+        // check the input if is a number
+        do
+        {
+            Console.Write("Enter a number: ");
+            input = Console.ReadLine();
+
+        } while (!int.TryParse(input, out num) && (num != -1));
+
+
+        bool isTrue = true;
+        for (int i = 2; (i < num) && isTrue; i++)
+        {
+            if (num % i == 0)
+            {
+                isTrue = false;
+            }
+        }
+
+        // output the result
+        if (isTrue)
+        {
+            Console.WriteLine(input + " is a prime number");
+        }
+        else
+        {
+            Console.WriteLine(input + " is not a prime number");
+        }
+    }
+}
